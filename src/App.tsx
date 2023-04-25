92960a3f23567c3217c086e933c4cd9cd54b3e35
GogoWwz <wwz_zjut_web@163.com>
2023-04-25 22:40:23 +0800

    feat: 1

---
+++ src/App.tsx
@@ -0,0 +1,302 @@
+import React from 'react'
+import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
+import Flip from 'react-reveal/Flip';
+import Roll from 'react-reveal/Roll';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import {
+  HeaderConfig,
+  Section1Config,
+  Section2Config,
+  Section3Config,
+  Section4Config,
+  Section5Config,
+  Section6Config,
+  FooterConfig
+} from './config';
+import Img1 from './assets/index/bg_banner@2x.png';
+import PrevImg from './assets/index/prev.png'
+import NextImg from './assets/index/next.png'
+import IconArrow from './assets/index/icon_arrow.png'
+import 'swiper/swiper.css';
+import './App.scss'
+
+function App() {
+  return (
+    <div className='app'>
+      {/* 头部 */}
+      <header className='header'>
+        <nav className='header-nav'>
+          <img className='header-nav-logo' src={HeaderConfig.logo} alt="" />
+          <div className='header-nav-tabs'>
+            {
+              HeaderConfig.tabs.map((tab, index) => {
+                return (
+                  <div className={`header-nav-tab ${index === 0 ? 'active' : ''}`} key={index}>
+                    {tab}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </nav>
+
+        <div className='header-wrapper'>
+          <img src={Img1} alt="" />
+          <div className='header-info'>
+            <div>歆会广告</div>
+            <div>让广告变现更有效率</div>
+            <div>歆会广告联盟创立于2016年，建有国内响应速度快、容量高、技术先进的移动广告网络交易中心与大数据计算平台，全面支持开屏、横幅、插屏、原生、激励视频等各种主流的移动广告形式。</div>
+          </div>
+        </div>
+      </header>
+
+      {/* 正文1 */}
+      <Fade>
+        <section className='section1'>
+          <h1>{Section1Config.title}</h1>
+          <div className='section1-content'>
+            {
+              Section1Config.content.map((item, index) => {
+                const { img, title, desc } = item;
+                return (
+                  <Zoom key={index}>
+                    <div className='section1-item'>
+                      <img src={img} alt="" />
+                      <div>{title}</div>
+                      <div>{desc}</div>
+                    </div>
+                  </Zoom>
+                );
+              })
+            }
+          </div>
+        </section>
+      </Fade>
+
+      {/* 正文2 */}
+      <Fade>
+        <section className='section2'>
+          <h1>{Section2Config.title}</h1>
+          <div className='section2-content'>
+            {
+              Section2Config.content.map((item, index) => {
+                const { img, title, desc } = item;
+                return (
+                  <Zoom key={index}>
+                    <div className='section2-item' key={index}>
+                      <img src={img} alt="" />
+                      <div>{title}</div>
+                      <div>{desc}</div>
+                    </div>
+                  </Zoom>
+                );
+              })
+            }
+          </div>
+        </section>
+      </Fade>
+
+      {/* 正文3 */}
+      <Zoom>
+        <section className='section3'>
+          <div className='section3-part1'>
+            {
+              Section3Config.content.map((item, index) => {
+                const { title, img, desc } = item;
+                return (
+                  <div className="section3-part1-item" key={index}>
+                    <img src={img} alt="" />
+                    <div>{title}</div>
+                    <div>{desc}</div>
+                  </div>
+                )
+              })
+            }
+          </div>
+
+          <div className='section3-title'>{Section3Config.title}</div>
+
+          <div className='section3-carousel'>
+            <img className='swiper-button-prev' src={PrevImg} alt="" />
+            <div className='section3-carousel-wrapper'>
+              <Swiper
+                loop
+                slideToClickedSlide
+                spaceBetween={20}
+                slidesPerView={6}
+                modules={[Navigation]}
+                navigation={{
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                }}
+                effect="coverflow"
+                coverflowEffect={{
+                  rotate: 0,
+                  modifier: 1,
+                  slideShadows: true,
+                  stretch: 131,
+                  depth: 300,
+                }}
+              >
+                {
+                  [...Section3Config.banners, ...Section3Config.banners].map((item, index) => {
+                    return (
+                      <SwiperSlide key={index}><img src={item} /></SwiperSlide>
+                    )
+                  })
+                }
+              </Swiper>
+            </div>
+            <img className='swiper-button-next' src={NextImg} alt=""/>
+          </div>
+        </section>
+      </Zoom>
+
+      {/* 正文4 */}
+      <Fade>
+        <section className='section4'>
+          <div className="section4-wrapper">
+            <Flip>
+              <div>{Section4Config.title}</div>
+            </Flip>
+            <Flip>
+              <div>{Section4Config.desc}</div>
+            </Flip>
+          </div>
+        </section>
+      </Fade>
+
+      {/* 正文5 */}
+      <Zoom>
+        <section className='section5'>
+          <div className="section5-part1">
+            <div className="section5-title">{Section5Config.part1.title}</div>
+            <div className="section5-part1-content">
+              {
+                Section5Config.part1.content.map((item, index) => {
+                  const { title, img, desc } = item;
+                  return (
+                    <div className="section5-part1-item" key={index}>
+                      <img src={img} alt="" />
+                      <div>{title}</div>
+                      <div dangerouslySetInnerHTML={{ __html: desc }} />
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className="section5-part2">
+            <div className="section5-title">{Section5Config.part2.title}</div>
+            <div className="section5-subTitle">{Section5Config.part2.subTitle}</div>
+            <div className="section5-part2-content">
+              {
+                Section5Config.part2.content.map((item, index) => {
+                  const { img, desc } = item;
+                  return (
+                    <React.Fragment key={index}>
+                      <div className="section5-part2-item">
+                        <img src={img} alt="" />
+                        <div>{desc}</div>
+                      </div>
+                      {
+                        index < Section5Config.part2.content.length - 1 &&
+                        <img width={34} height={34} src={IconArrow} alt="" />
+                      }
+                    </React.Fragment>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className="section5-part3">
+            <div className="section5-title">{Section5Config.part3.title}</div>
+            <div className='section5-part3-content'>
+              {
+                Section5Config.part3.content.map((item, index) => {
+                  const { img, desc } = item;
+                  return (
+                    <div className="section5-part3-item" key={index}>
+                      <img src={img} alt="" />
+                      <div>{desc}</div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </section>
+      </Zoom>
+
+      {/* 正文6 */}
+      <Fade>
+        <section className='section6'>
+          <div className="section6-title">{Section6Config.title}</div>
+          <div className="section6-content">
+            {
+              Section6Config.content.map((item, index) => {
+                const { img, desc, info, icon } = item;
+                return (
+                  <div className="section6-item" key={index}>
+                    <div className="section6-item-top">
+                      <img src={img} alt="" />
+                      <div>{info}</div>
+                      <span>more&gt;</span>
+                    </div>
+
+                    <div className="section6-item-bottom">
+                      <img src={icon} alt="" />
+                      <div>{desc}</div>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </section>
+      </Fade>
+
+      {/* 底部 */}
+      <footer className='footer'>
+        <div className="footer-wrapper">
+          <img src={FooterConfig.logo} className="footer-logo" />
+          <div className="footer-about">
+            <div className="footer-about-title">{FooterConfig.about.title}</div>
+            <div className="footer-about-content">
+              {
+                FooterConfig.about.content.map((item, index) => {
+                  return (
+                    <div key={index} className='footer-about-item'>
+                      {item}
+                    </div>
+                  );
+                })
+              }
+            </div>
+          </div>
+          <div className="footer-connect">
+
+            <div className="footer-connect-title">{FooterConfig.connect.title}</div>
+            <div className="footer-connect-content">
+              {
+                FooterConfig.connect.content.map((item, index) => {
+                  const { icon, text } = item;
+                  return (
+                    <div key={index} className='footer-connect-item'>
+                      <img src={icon} alt="" />
+                      <span>{text}</span>
+                    </div>
+                  );
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/config.ts
@@ -0,0 +1,324 @@
+import LogoHeader from './assets/index/logo_header.png'
+
+import SectionImg1_1 from './assets/index/section1_1.png'
+import SectionImg1_2 from './assets/index/section1_2.png'
+import SectionImg1_3 from './assets/index/section1_3.png'
+import SectionImg1_4 from './assets/index/section1_4.png'
+
+import SectionImg2_1 from './assets/index/section2_1.png'
+import SectionImg2_2 from './assets/index/section2_2.png'
+import SectionImg2_3 from './assets/index/section2_3.png'
+import SectionImg2_4 from './assets/index/section2_4.png'
+import SectionImg2_5 from './assets/index/section2_5.png'
+import SectionImg2_6 from './assets/index/section2_6.png'
+import SectionImg2_7 from './assets/index/section2_7.png'
+import SectionImg2_8 from './assets/index/section2_8.png'
+
+import SectionImg3_1 from './assets/index/section3_1.png'
+import SectionImg3_2 from './assets/index/section3_2.png'
+import SectionImg3_3 from './assets/index/section3_3.png'
+
+import SectionImg3_4 from './assets/index/section3_4.png'
+import SectionImg3_5 from './assets/index/section3_5.png'
+import SectionImg3_6 from './assets/index/section3_6.png'
+import SectionImg3_7 from './assets/index/section3_7.png'
+import SectionImg3_8 from './assets/index/section3_8.png'
+import SectionImg3_9 from './assets/index/section3_9.png'
+
+import SectionImg5_1 from './assets/index/section5_1.png'
+import SectionImg5_2 from './assets/index/section5_2.png'
+import SectionImg5_3 from './assets/index/section5_3.png'
+import SectionImg5_4 from './assets/index/section5_4.png'
+
+import SectionImg5_5 from './assets/index/section5_5.png'
+import SectionImg5_6 from './assets/index/section5_6.png'
+import SectionImg5_7 from './assets/index/section5_7.png'
+import SectionImg5_8 from './assets/index/section5_8.png'
+import SectionImg5_9 from './assets/index/section5_9.png'
+import SectionImg5_10 from './assets/index/section5_10.png'
+import SectionImg5_11 from './assets/index/section5_11.png'
+import SectionImg5_12 from './assets/index/section5_12.png'
+
+import SectionImg5_13 from './assets/index/section5_13.png'
+import SectionImg5_14 from './assets/index/section5_14.png'
+import SectionImg5_15 from './assets/index/section5_15.png'
+import SectionImg5_16 from './assets/index/section5_16.png'
+import SectionImg5_17 from './assets/index/section5_17.png'
+import SectionImg5_18 from './assets/index/section5_18.png'
+
+import SectionImg6_1 from './assets/index/section6_1.png'
+import SectionImg6_2 from './assets/index/section6_2.png'
+import SectionImg6_3 from './assets/index/section6_3.png'
+import SectionImg6_4 from './assets/index/section6_4.png'
+
+import LogoFooter from './assets/index/logo_footer.png'
+import FooterImg1 from './assets/index/footer_1.png'
+import FooterImg2 from './assets/index/footer_2.png'
+import FooterImg3 from './assets/index/footer_3.png'
+
+export const HeaderConfig = {
+  logo: LogoHeader,
+  tabs: [
+    '首页',
+    '流量变现',
+    'APP开发',
+    '联系我们'
+  ],
+};
+
+export const Section1Config = {
+  title: '歆会优势',
+  content: [
+    {
+      img: SectionImg1_1,
+      title: '海量的广告主',
+      desc: '广告主涵盖（电商、游戏、金融等）多个行业，多家广告主持续投放确保长期超高填充率。'
+    },
+    {
+      img: SectionImg1_2,
+      title: '丰富的媒体资源',
+      desc: '以平台独家资源和各大媒体矩阵为核心，甄选品质流量，覆盖上万家跨屏媒体，涵盖多层次的高价值用户人群。'
+    },
+    {
+      img: SectionImg1_3,
+      title: '精准投放',
+      desc: '海量的用户信息及全方位的人群画像，客户可'
+    },
+    {
+      img: SectionImg1_4,
+      title: '高额回报',
+      desc: '充分反馈开发者合作伙伴；支持展现、点击、'
+    }
+  ]
+};
+
+
+export const Section2Config = {
+  title: '丰富的广告样式',
+  content: [
+    {
+      img: SectionImg2_1,
+      title: '开屏广告',
+      desc: '开屏以app启动作为曝光时机，提供5s的可感知广告展示。'
+    },
+    {
+      img: SectionImg2_2,
+      title: '全屏视频',
+      desc: '5秒可跳过，高收益的同时兼顾用户感受。'
+    },
+    {
+      img: SectionImg2_3,
+      title: '插屏广告',
+      desc: '在应用内自然停顿和切换时展示的半屏广告形式，用户可点击素材跳转广告。'
+    },
+    {
+      img: SectionImg2_4,
+      title: '信息流广告',
+      desc: '充分反馈开发者合作伙伴；支持展现、点击、可内嵌于应用内容中显示。支持模版渲染和自渲染'
+    },
+    {
+      img: SectionImg2_5,
+      title: 'Banner广告',
+      desc: '在app顶/中/底部悬浮且可关闭的边栏广告'
+    },
+    {
+      img: SectionImg2_6,
+      title: '激励视频',
+      desc: '将短视频融入app场景中，完整观看视频广告以免费获得奖励的广告形式'
+    },
+    {
+      img: SectionImg2_7,
+      title: '语音红包',
+      desc: '语音激励广告平台，固定位激励场景合作，预算足，填充高，变现效果好。'
+    },
+    {
+      img: SectionImg2_8,
+      title: '互动广告',
+      desc: '互动广告以巧妙构思和与计算机高新技术的融合，获得难以想象的展示效果，使广告深入人心。'
+    }
+  ]
+}
+
+export const Section3Config = {
+  content: [
+    {
+      img: SectionImg3_1,
+      title: '运营总监',
+      desc: '与XHADS合作虽然不久，XHADS的专业性与多年变现经验为了我们提供了不少的便捷，广告效果和收益都得到了很大提升，让我司能更集中精力在开发产品上。希望继续保持这种合作关系，共同创造更高的利益。'
+    },
+    {
+      img: SectionImg3_2,
+      title: '开发者',
+      desc: '作为一个独立开发者，在合作两年多里我感受到XHADS的专业和认真，公司对接团队务实高效，技术水平高，业务形态丰富，热点问题总能及时解决。祝愿XHADS越来越强 ！'
+    },
+    {
+      img: SectionImg3_3,
+      title: '商业化负责人',
+      desc: '在合作3年多的时间里，感受到XHADS是一家业内非常专业的帮助开发者进行流量变现的公司，公司对接团队务实高效，技术水平高，业务形态丰富，帮助我们实现了价值最大化，期待以后长期合作，共赢发展！'
+    }
+  ],
+  title: '多家知名DSP广告平台与XHADS合作',
+  banners: [
+    SectionImg3_4,
+    SectionImg3_5,
+    SectionImg3_6,
+    SectionImg3_7,
+    SectionImg3_8,
+    SectionImg3_9
+  ]
+}
+
+export const Section4Config = {
+  title: '行业解决方案，从歆会开始',
+  desc: '我们面向商城，盲盒，购物，地产，汽车出行，金融等专业，提供全方位APP开发，小程序开发等解决方案'
+};
+
+export const Section5Config = {
+  part1: {
+    title: '为什么选择我们',
+    content: [
+      {
+        img: SectionImg5_1,
+        title: '公司实力',
+        desc: '杭州歆会科技有限公司，作为以互联网高端建设业务为核心的专业网络技术服务供应商，主营业务有PC端高端网站定制开发、手机端APP定制开发、小程序制作以及数字营销等，在品牌网站建设、SEO营销型网站制作、WAP手机网站、网站建设项目外包、软件开发等项目上具有丰富的经验。'
+      },
+      {
+        img: SectionImg5_2,
+        title: '客户案例',
+        desc: '我们歆会5年的开发经验，每月有上百个项目产出，可以为项目提供借鉴和参考，优中更优；像华电、华为、云集、圣元等这些集团公司都是我们的合作伙伴。'
+      },
+      {
+        img: SectionImg5_3,
+        title: '团队规模',
+        desc: '<b>500+</b>专业技术团队，<b>40</b>多技术管理精英，高级技术工程师占比<b>30%</b>，累计为<b>10000+</b>的政企单位提供过解决方案'
+      },
+      {
+        img: SectionImg5_4,
+        title: '公司实力',
+        desc: '项目交付后，有标准的售后流程和售后响应机制，专门的售后部门及时处理，为客户提供强有力的保障'
+      },
+    ],
+  },
+
+  part2: {
+    title: '服务流程',
+    subTitle: '标准化的开发流程，保证产品交付质量和工期',
+    content: [
+      {
+        img: SectionImg5_5,
+        desc: '需求洽谈分析'
+      },
+      {
+        img: SectionImg5_6,
+        desc: '产品架构'
+      },
+      {
+        img: SectionImg5_7,
+        desc: 'UE原型设计'
+      },
+      {
+        img: SectionImg5_8,
+        desc: 'UI视觉设计'
+      },
+      {
+        img: SectionImg5_9,
+        desc: '产品研发'
+      },
+      {
+        img: SectionImg5_10,
+        desc: '产品测试'
+      },
+      {
+        img: SectionImg5_11,
+        desc: '产品上线'
+      },
+      {
+        img: SectionImg5_12,
+        desc: '产品运维'
+      },
+    ]
+  },
+
+  part3: {
+    title: '成功案例',
+    content: [
+      {
+        img: SectionImg5_13,
+        desc: '企业网站建设｜网站设计'
+      },
+      {
+        img: SectionImg5_14,
+        desc: '小程序开发｜小程序制作'
+      },
+      {
+        img: SectionImg5_15,
+        desc: '小程序开发｜小程序制作'
+      },
+      {
+        img: SectionImg5_16,
+        desc: 'APP开发｜APP制作'
+      },
+      {
+        img: SectionImg5_17,
+        desc: '企业网站建设｜网站设计'
+      },
+      {
+        img: SectionImg5_18,
+        desc: '软件开发｜系统开发｜平台开发'
+      }
+    ]
+  }
+};
+
+export const Section6Config = {
+  title: 'App开发技术问答',
+  content: [
+    {
+      img: SectionImg6_4,
+      info: '一般开发一款App大致需要数个月，具体时间要根据App开发的功能需求量计算。歆会的混合App开发技术，可一套代码司时生成Android与iOS两端App，同时平台积累了大量成熟的app功能模块，…',
+      icon: SectionImg6_1,
+      desc: '做一个App需要多久？'
+    },
+    {
+      img: SectionImg6_4,
+      info: 'App开发的工期和费用估算需视功能需求的多少和难易程度而定。在歆会，一对一的专业的顾问将逐步引导您梳理需求，让您更加清楚自己的想法转化为App需要什么功能，最终根据功能...',
+      icon: SectionImg6_2,
+      desc: '做一个App多少钱？'
+    },
+    {
+      img: SectionImg6_4,
+      info: '目前歆会上已累计创建应用超过95万个，服务客户包括Intel、海尔、中兴、上汽集团、碧桂园、中信集团等诸多国内知名企业，业务能力涵盖loT、在线教育、直播、O20、企业办公管理、…',
+      icon: SectionImg6_3,
+      desc: '你们开发过哪些案例？'
+    }
+  ],
+};
+
+export const FooterConfig = {
+  logo: LogoFooter,
+  about: {
+    title: '关于我们',
+    content: [
+      '杭州歆会科技有限公司',
+      '地址：杭州市余杭区仓前街道绿创广场3号楼B座11楼',
+      '邮编：310000'
+    ]
+  },
+  connect: {
+    title: '联系我们',
+    content: [
+      {
+        icon: FooterImg1,
+        text: '13777866700'
+      },
+      {
+        icon: FooterImg2,
+        text: '740626674@qq.com'
+      },
+      {
+        icon: FooterImg3,
+        text: '740626674'
+      }
+    ],
+  },
+};
